317b62d236f0df83ba037ee286a2b52efa587fe4
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:17:00 +0300

    msix: migrate to Conan v2

--- recipes/msix/all/conanfile.py
+++ recipes/msix/all/conanfile.py
@@ -67,6 +67,16 @@ class MsixConan(ConanFile):
             # with_xerces is required
             del self.options.with_xerces
 
+        if self.options.xml_parser == "auto":
+            if self.settings.os == "Android":
+                self.options.xml_parser = "javaxml"
+            elif is_apple_os(self):
+                self.options.xml_parser = "applexml"
+            elif self.settings.os == "Windows":
+                self.options.xml_parser = "msxml6"
+            else:
+                self.options.xml_parser = "xerces"
+
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
