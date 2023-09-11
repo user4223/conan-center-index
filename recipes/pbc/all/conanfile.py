46d1f661e61c3c870b9e55da2defdc6d8de5fd5e
Martin Valgur <martin.valgur@gmail.com>
2023-09-11 18:28:22 +0300

    pbc: use winflexbison on Windows

--- recipes/pbc/all/conanfile.py
+++ recipes/pbc/all/conanfile.py
@@ -33,6 +33,10 @@ class PbcConan(ConanFile):
         "fPIC": True,
     }
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def export_sources(self):
         export_conandata_patches(self)
 
