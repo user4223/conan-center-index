0b41fa2ddf758e23cd65eab4d3e8329737c7c05f
Martin Valgur <martin.valgur@gmail.com>
2023-09-12 23:12:57 +0300

    gst-libav: bump deps

--- recipes/gst-libav/all/conanfile.py
+++ recipes/gst-libav/all/conanfile.py
@@ -124,7 +124,7 @@ class GStLibAVConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("glib/2.77.1")
+        self.requires("glib/2.78.0")
         self.requires(f"gstreamer/{self.version}")
         self.requires(f"gst-plugins-base/{self.version}")
         self.requires("ffmpeg/6.0")
@@ -201,7 +201,7 @@ class GStLibAVConan(ConanFile):
                     shutil.move(filename_old, filename_new)
 
     def package(self):
-        copy(self, pattern="COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         meson = Meson(self)
         meson.install()
         self._fix_library_names(os.path.join(self.package_folder, "lib"))
