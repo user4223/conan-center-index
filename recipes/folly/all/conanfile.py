7cbfa48d0bae50839b416e0ee97b0e8fd8eda121
Martin Valgur <martin.valgur@gmail.com>
2023-11-14 09:47:47 +0200

    folly: use is_apple_os()

--- recipes/folly/all/conanfile.py
+++ recipes/folly/all/conanfile.py
@@ -112,7 +112,7 @@ class FollyConan(ConanFile):
         if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
-        if self.settings.os == "Macos" and self.settings.arch != "x86_64":
+        if is_apple_os(self) and self.settings.arch != "x86_64":
             raise ConanInvalidConfiguration("Conan currently requires a 64bit target architecture for Folly on Macos")
 
         if self.settings.os == "Windows" and self.settings.arch != "x86_64":
