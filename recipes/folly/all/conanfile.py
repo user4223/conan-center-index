4387210c77909d4c1be8563e8d0b57511dc7b888
Pau Farre <pau.farre@esperantotech.com>
2024-01-14 19:41:03 +0000

    Do not allow static glog if building shared folly

--- recipes/folly/all/conanfile.py
+++ recipes/folly/all/conanfile.py
@@ -122,6 +122,10 @@ class FollyConan(ConanFile):
             # https://github.com/facebook/folly/issues/2266
             raise ConanInvalidConfiguration(f"{self.ref} could not be built by apple-clang < 14.0. Use apple-clang >= 14.0")
 
+        glog = self.dependencies["glog"]
+        if self.options.shared and not glog.options.shared:
+            raise ConanInvalidConfiguration(f"If Folly is built as shared lib, glog must be a shared lib too.")
+        
         boost = self.dependencies["boost"]
         if boost.options.header_only:
             raise ConanInvalidConfiguration(f"{self.ref} could not be built with a header only Boost. Use -o 'boost/*:header_only=False'")
