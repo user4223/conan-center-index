bce8fe8c1427fe347d1b30f2bcec06b35440bcd0
Martin Valgur <martin.valgur@gmail.com>
2024-01-23 20:02:22 +0200

    libjxl: disable MSVC Debug build for v0.6.1

--- recipes/libjxl/all/conanfile.py
+++ recipes/libjxl/all/conanfile.py
@@ -80,6 +80,9 @@ class LibjxlConan(ConanFile):
     def validate(self):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, 11)
+        if self.version == "0.6.1" and is_msvc(self) and self.settings.build_type == "Debug":
+            # Fails with a missing DLL error in test_package
+            raise ConanInvalidConfiguration(f"{self.ref} does not support Debug builds with MSVC")
 
     def build_requirements(self):
         # Require newer CMake, which allows INCLUDE_DIRECTORIES to be set on INTERFACE targets
