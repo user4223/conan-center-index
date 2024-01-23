fefd7b2200858731355d060515b301d00f266e95
czoido <mrgalleta@gmail.com>
2024-01-23 15:21:33 +0100

    do not convert LIBRARY to LIBRARIES

--- recipes/openscenegraph/all/conanfile.py
+++ recipes/openscenegraph/all/conanfile.py
@@ -270,8 +270,6 @@ class OpenSceneGraphConanFile(ConanFile):
                 # Don't replace in the root dir
                 continue
             content = path.read_text()
-            # Correct usage of *_LIBRARY variables to *_LIBRARIES
-            content = content.replace("_LIBRARY", "_LIBRARIES")
             # Allow explicit control of plugins via OSG_WITH_* variables
             # e.g. replace IF(FFMPEG_FOUND) with IF(OSG_WITH_FFMPEG)
             content = re.sub(r"\b([A-Z]+)_FOUND\b", r"OSG_WITH_\1", content)
