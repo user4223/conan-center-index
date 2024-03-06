0bc610ed48a83a8cd643a5114e35c34400b84136
Andrew Marshall <planetmarshalluk@gmail.com>
2024-03-06 23:55:12 +0000

    [llvm-core] resolve v1 hooks issues

--- recipes/llvm-core/all/conanfile.py
+++ recipes/llvm-core/all/conanfile.py
@@ -430,6 +430,7 @@ class LLVMCoreConan(ConanFile):
         cmake.install()
         package_folder = Path(self.package_folder)
         rmdir(self, package_folder / "lib" / "cmake")
+        rmdir(self, package_folder / "share")
 
         build_info = self._write_build_info()
 
