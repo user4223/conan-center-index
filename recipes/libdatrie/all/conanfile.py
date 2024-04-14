0af94b38e9c1b9d760ccd425669b14204cb2cfaa
Martin Valgur <martin.valgur@gmail.com>
2024-04-14 13:24:06 +0300

    libdatrie: fix Windows build

--- recipes/libdatrie/all/conanfile.py
+++ recipes/libdatrie/all/conanfile.py
@@ -9,7 +9,6 @@ from conan.tools.files import apply_conandata_patches, copy, export_conandata_pa
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, unix_path
-from conans.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.54.0"
 
