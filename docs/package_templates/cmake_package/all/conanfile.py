6ce6752a6e7ca78b531df48bf4d47ae15498ba6d
Martin Valgur <martin.valgur@gmail.com>
2024-07-27 15:09:06 +0300

    Move CMake and and PkgConfig properties to the top, drop legacy generator props
    The CMake and PkgConfig info is the most relevant part to any consumers and should come first, imo.
    
    New recipes should not bother with the legacy generators, especially after test_v1_package is no longer included.

--- docs/package_templates/cmake_package/all/conanfile.py
+++ docs/package_templates/cmake_package/all/conanfile.py
@@ -122,6 +122,8 @@ class PackageConan(ConanFile):
         # if package provides a pkgconfig file (package.pc, usually installed in <prefix>/lib/pkgconfig/)
         self.cpp_info.set_property("pkg_config_name", "package")
 
+        self.cpp_info.libs = ["package_lib"]
+
         # If they are needed on Linux, m, pthread and dl are usually needed on FreeBSD too
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
