8d6e9a77905844c65b0240b8a56c2ae4345f4026
Martin Valgur <martin.valgur@gmail.com>
2024-07-29 12:04:55 +0300

    fbgemm: can use a lower C_STANDARD

--- recipes/fbgemm/all/conanfile.py
+++ recipes/fbgemm/all/conanfile.py
@@ -92,6 +92,7 @@ class FbgemmConan(ConanFile):
         tc.variables["FBGEMM_BUILD_DOCS"] = False
         if not valid_min_cppstd(self, self._min_cppstd):
             tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
+        tc.variables["CMAKE_C_STANDARD"] = 99
         tc.generate()
 
         deps = CMakeDeps(self)
