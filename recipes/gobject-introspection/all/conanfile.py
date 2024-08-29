6cdc163f3229160dc76a791079f0731c2ef326a5
Martin Valgur <martin.valgur@gmail.com>
2024-08-29 22:25:46 +0300

    gobject-introspection: add libffi dep

--- recipes/gobject-introspection/all/conanfile.py
+++ recipes/gobject-introspection/all/conanfile.py
@@ -2,8 +2,9 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import cross_building
-from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv, Environment
 from conan.tools.files import copy, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
