9cecaa2db7a6c6142d5ef9aa675836588a875554
Martin Valgur <martin.valgur@gmail.com>
2024-08-30 11:12:34 +0300

    gobject-introspection: export GI_GIR_PATH env var

--- recipes/gobject-introspection/all/conanfile.py
+++ recipes/gobject-introspection/all/conanfile.py
@@ -190,6 +190,8 @@ class GobjectIntrospectionConan(ConanFile):
         self.buildenv_info.define_path("GI_GIR_PATH", os.path.join(self.package_folder, "res", "gir-1.0"))
         self.buildenv_info.define_path("GI_TYPELIB_PATH", os.path.join(self.package_folder, "lib", "girepository-1.0"))
 
+        self.buildenv_info.append_path("GI_GIR_PATH", os.path.join(self.package_folder, "res", "gir-1.0"))
+
         # TODO: remove in conan v2
         bin_path = os.path.join(self.package_folder, "bin")
         self.env_info.PATH.append(bin_path)
